68ce093dc35ed34096cf4208c9138f0d9fb0f2e1
Lucas Sarasa <lucasmsarasa@gmail.com>
2026-07-15 19:24:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="">My App</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
