7f28ef2b4d8313e90700e5a8f8791cad3331265d
Lucas Sarasa <lucasmsarasa@gmail.com>
2026-07-16 22:05:42 -0300

    chore: adiciona eslint-plugin-simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,29 @@
 import js from "@eslint/js";
-import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 export default defineConfig([
   globalIgnores(["dist", "src/components/ui"]),
   {
     files: ["**/*.{js,jsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
     languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
